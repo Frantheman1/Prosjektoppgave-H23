30f609efd16070cdf2b7a2de9a210ca0023f76c1
ValentinRStoyanov <valentin.r.stoyanov@gmail.com>
2023-10-30 23:11:58 +0100

    adda til package json, også lagde en babelrc

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,4 +1,33 @@
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Component } from 'react-simplified';
-import { HashRouter, Route } from 'react-router-dom';
+import { NavBar} from './widgets';
+
+
+class Noe extends  Component {
+
+ render() {
+  return (
+   <>
+   
+
+    <div>Noe</div> 
+   </>
+    );
+
+
+ 
+
+ 
+ }
+}
+
+
+
+let root = document.getElementById('root');
+if (root)
+  createRoot(root).render(
+    <>
+    <Noe />
+    </>,
+  );
