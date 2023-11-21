3b55d126304bb0e1b28920d734632f61a12e080d
Frantheman1 <115075212+Frantheman1@users.noreply.github.com>
2023-11-21 16:10:44 +0100

    Delete server/config.ts

--- server/config.ts
+++
@@ -1,4 +0,0 @@
-process.env.MYSQL_HOST = 'mysql.stud.ntnu.no';
-process.env.MYSQL_USER = 'kineawe_gruppe';
-process.env.MYSQL_PASSWORD = 'BesteGruppa2023+';
-process.env.MYSQL_DATABASE = 'kineawe_gruppe_dev';
